504f8de254c13e4cc827e3a3dae476709be139a6
DamianPurgal <damix242@gmail.com>
2022-08-29 08:49:34 +0200

    Project, DB, flyway configuration

---
+++ src/main/java/pl/damian/demor/DemorApplication.java
@@ -0,0 +1,13 @@
+package pl.damian.demor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/damian/demor/DemorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.damian.demor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
